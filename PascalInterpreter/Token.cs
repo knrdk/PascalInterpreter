35a0ef46f600d0c9336eb7ff69fa6fa87a96156b
Konrad Kopciuch <konrad1991@gmail.com>
2016-09-28 19:38:33 +0200

    Develop (#1)
    * initial structure
    
    * created Token model
    
    * change project structure
    
    * adjusted namespaces after refactoring
    
    * parsing program into tokens
    
    * added constructor without value for token
    
    * added Eat and Expr methods

---
+++ PascalInterpreter/Token.cs
@@ -0,0 +1,20 @@
+﻿namespace PascalInterpreter
+{
+    public class Token
+    {
+        public TokenType Type { get; }
+        public string Value { get; }
+
+        public Token(TokenType type)
+        {
+            Type = type;
+        }
+
+        public Token(TokenType type, string value) : this(type)
+        {
+            Value = value;
+        }
+
+        public override string ToString() => $"Token({Type}: {Value})";
+    }
+}
---
+++ PascalInterpreter/TokenType.cs
@@ -0,0 +1,11 @@
+﻿namespace PascalInterpreter
+{
+    public enum TokenType
+    {
+        Unknown = 0,
+        Integer,
+        Plus,
+        Minus,
+        EndOfFile
+    }
+}
